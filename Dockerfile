d6bc0aac259611e3d5fe32678a5dab31528e4d7d
vtrushin <monospectr@gmail.com>
2017-11-01 04:12:30 +0300

    test

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,21 @@
 FROM node:boron
+RUN mkdir /data
+WORKDIR /data
+ADD package.json /data
+RUN npm install
+RUN npm install nodemon -g
+ADD . /data
 
 # Create app directory
 # WORKDIR /usr/src/app
 
 # Install app dependencies
-COPY package.json .
+# COPY package.json .
 # For npm@5 or later, copy package-lock.json as well
 # COPY package.json package-lock.json ./
 
-RUN npm install
-
 # Bundle app source
-COPY . .
+# COPY . .
 
-EXPOSE 8080
-CMD ["npm", "start"]
+# EXPOSE 8080
+# CMD ["npm", "start"]
--- server.js
+++ server.js
@@ -1,17 +1,48 @@
 'use strict';
 
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+// const PORT = 8080;
+// const HOST = '0.0.0.0';
+
+// console.log(process.env.DATABASE_URL);
 
 // App
-const app = express();
-app.get('/', (req, res) => {
-    res.send('Хомяк 😋👩‍👩‍👧👩‍👩‍👧‍👦 пыщка\n');
+// const app = express();
+// app.get('/', (req, res) => {
+//     res.send('Хомяк 😋👩‍👩‍👧👩‍👩‍👧‍👦 пыщка assad2d3\n');
+// });
+//
+// app.listen(PORT, HOST);
+//
+// console.log(`Running on http://${HOST}:${PORT}`);
+
+// var URL = 'mongodb://localhost:27017/mydatabase';
+//
+
+console.log('DATABASE_URL', process.env.DATABASE_URL);
+
+MongoClient.connect(process.env.DATABASE_URL, function (err, db) {
+    if (err) return;
+
+    db.collection('foods').findOne({ name: 'taco' }).then(a => {
+        console.log(a);
+    });
+
+    // var collection = db.collection('foods');
+    //
+    // collection.find({
+    //     name: 'taco'
+    // }).next();
+
+    db.close();
+
+    // collection.insert({name: 'taco', tasty: true}, function (err, result) {
+    //     collection.find({name: 'taco'}).toArray(function (err, docs) {
+    //         console.log(docs[0]);
+    //         db.close();
+    //     })
+    // })
 });
-
-app.listen(PORT, HOST);
-
-console.log(`Running on http://${HOST}:${PORT}`);
