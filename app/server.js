6fd35770f1bbab6bce6baaaf21169d4cb21b1ea3
vtrushin <monospectr@gmail.com>
2017-11-02 01:41:53 +0300

    test

--- app/server.js
+++ app/server.js
@@ -9,40 +9,40 @@ const MongoClient = require('mongodb').MongoClient;
 
 // console.log(process.env.DATABASE_URL);
 
-// App
-// const app = express();
-// app.get('/', (req, res) => {
-//     res.send('Хомяк 😋👩‍👩‍👧👩‍👩‍👧‍👦 пыщка assad2d3\n');
-// });
-//
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Хомяк 😋👩‍👩‍👧👩‍👩‍👧‍👦 пыщка assad2d3\n');
+});
+
 // app.listen(PORT, HOST);
-//
+
 // console.log(`Running on http://${HOST}:${PORT}`);
 
 // var URL = 'mongodb://localhost:27017/mydatabase';
 //
 
-console.log('DATABASE_URL', process.env.DATABASE_URL);
-
-MongoClient.connect(process.env.DATABASE_URL, function (err, db) {
-    if (err) return;
-
-    db.collection('foods').findOne({ name: 'taco' }).then(a => {
-        console.log(a);
-    });
-
-    // var collection = db.collection('foods');
-    //
-    // collection.find({
-    //     name: 'taco'
-    // }).next();
-
-    db.close();
-
-    // collection.insert({name: 'taco', tasty: true}, function (err, result) {
-    //     collection.find({name: 'taco'}).toArray(function (err, docs) {
-    //         console.log(docs[0]);
-    //         db.close();
-    //     })
-    // })
-});
+// console.log('DATABASE_URL', process.env.DATABASE_URL);
+//
+// MongoClient.connect(process.env.DATABASE_URL, function (err, db) {
+//     if (err) return;
+//
+//     db.collection('foods').findOne({ name: 'taco' }).then(a => {
+//         console.log(a);
+//     });
+//
+//     // var collection = db.collection('foods');
+//     //
+//     // collection.find({
+//     //     name: 'taco'
+//     // }).next();
+//
+//     db.close();
+//
+//     // collection.insert({name: 'taco', tasty: true}, function (err, result) {
+//     //     collection.find({name: 'taco'}).toArray(function (err, docs) {
+//     //         console.log(docs[0]);
+//     //         db.close();
+//     //     })
+//     // })
+// });
